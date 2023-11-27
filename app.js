58bb14f040eac6500bc8df842943d676fa1c2f06
gpoquendo <againstthaflow13@protonmail.com>
2023-11-27 13:25:47 -0700

    Added index.html

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-     const connection = mysql.createConnection({
-       host: 'your_mysql_host',
-       user: 'your_mysql_user',
-       password: 'your_mysql_password',
-       database: 'your_database_name'
-     });
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-     connection.connect((err) => {
-       if (err) {
-         console.error('Error connecting to MySQL: ' + err.stack);
-         return;
-       }
-       console.log('Connected to MySQL as id ' + connection.threadId);
-     });
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'gpoquendo',
+    password: 'admin',
+    database: 'crud'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL: ' + err.stack);
+        return;
+    }
+    console.log('Connected to MySQL as id ' + connection.threadId);
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
